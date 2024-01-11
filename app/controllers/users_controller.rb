b425068c4149cd97c8d19493bd243071281e5c45
Osvani <osvacolijim@gmail.com>
2024-01-11 19:51:01 -0400

    Clean views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def my_portfolio
+    if user_signed_in?
+      @tracked_stocks = current_user.stocks
+    else
+      @tracked_stocks = []
+    end
   end
 end
