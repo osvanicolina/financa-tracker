f1b85e6f413789fa5c7dfe8bda0831ad59702bea
Osvani <osvacolijim@gmail.com>
2024-01-30 10:09:44 -0400

    Implement search friend structure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,23 @@ class UsersController < ApplicationController
       @tracked_stocks = []
     end
   end
+
+  def my_friends
+    @friends = current_user.friends
+  end
+
+  def search
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        render turbo_stream: turbo_stream.replace('results_turbo_stream', partial: 'friends/result')
+        else 
+            flash.now[:alert] = "Please enter a valid symbol to search"
+            render turbo_stream: turbo_stream.replace('results_turbo_stream', partial: 'friends/result')
+        end
+    else
+        flash.now[:alert] = "Please enter a symbol to search"
+        render turbo_stream: turbo_stream.replace('results_turbo_stream', partial: 'friends/result')
+    end
+  end
 end
